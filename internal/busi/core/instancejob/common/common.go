23c8f77d12ad9af6ab61711a23ca5b5d190b386c
cxytz01 <bluecxy@outlook.com>
2024-03-06 14:43:32 +0800

    chore: revamp code sequence

--- internal/busi/core/instancejob/common/common.go
+++ internal/busi/core/instancejob/common/common.go
@@ -37,6 +37,88 @@ const (
 
 type GetEventContentCallback func(string, *ethtypes.EthLog) string
 
+// EOA -> x contract(transaction)
+func TracingContractEventTXNCron(ctx context.Context, contractAddress, eventHash, eventName string, callEventContentFn GetEventContentCallback) error {
+	var (
+		maxHeightEvmReceipt fevm.EVMReceipt
+		recordedHeight      fevm.EventHeightCheckpoint
+	)
+	evmReceipts := make([]*fevm.EVMReceipt, 0)
+
+	recordedHeight.EventHash = eventHash
+	recordedHeight.EventName = eventName
+	recordedHeight.TxnType = TxnType
+
+	// select * from event_height_checkpoint where event_hash = %eventHash%;
+	// select max(height) from evm_receipt;
+	// select * from evm_receipt where height > leftHeight and height < rightHeight and logs like '%eventHash%' order by height desc;
+	if _, err := utils.X.Where("event_hash = ? and txn_type = ?", eventHash, TxnType).Get(&recordedHeight); err != nil {
+		log.Errorf("execute sql error: %v", err)
+		return err
+	}
+
+	if _, err := utils.X.Select("max(height) as height").Get(&maxHeightEvmReceipt); err != nil {
+		log.Errorf("execute sql error: %v", err)
+		return err
+	}
+
+	if maxHeightEvmReceipt.Height >= Finality {
+		maxHeightEvmReceipt.Height -= Finality
+	}
+
+	if err := utils.X.Where("height between ? and ? and \"to\" = ? and logs like ?", recordedHeight.MaxRecordedHeight+1, maxHeightEvmReceipt.Height, contractAddress, "%"+eventHash+"%").Asc("height").Find(&evmReceipts); err != nil {
+		log.Errorf("execute sql error: %v", err)
+		return err
+	}
+
+	for _, receipt := range evmReceipts {
+		logs := make([]ethtypes.EthLog, 0)
+		if err := json.Unmarshal([]byte(receipt.Logs), &logs); err != nil {
+			log.Warnf("Unmarshal receipt[height: %v] log err: %v", receipt.Height, err)
+			continue
+		}
+
+		for _, ethLog := range logs {
+			if ethLog.Topics[0].String() != eventHash {
+				continue
+			}
+
+			fevmEvent := fevm.FevmEvent{
+				ContractAddress: contractAddress,
+				Height:          uint64(receipt.Height),
+				TransactionHash: receipt.TransactionHash,
+				From:            receipt.From,
+				To:              receipt.To,
+				Status:          receipt.Status,
+				LogsBloom:       receipt.LogsBloom,
+				Logs:            receipt.Logs,
+				EventHash:       ethLog.Topics[0].String(),
+				EventName:       eventName,
+			}
+
+			fevmEvent.Note = callEventContentFn(eventName, &ethLog)
+
+			if _, err := utils.X.Insert(&fevmEvent); err != nil {
+				log.Errorf("execute sql error: %v", err)
+				return err
+			}
+
+			// update or insert event_height_checkoutpoint
+			recordedHeight.MaxRecordedHeight = uint64(receipt.Height)
+			if err := UpdateEventHeightCheckoutpoint(ctx, &recordedHeight); err != nil {
+				return err
+			}
+		}
+	}
+
+	recordedHeight.MaxRecordedHeight = uint64(maxHeightEvmReceipt.Height)
+	if err := UpdateEventHeightCheckoutpoint(ctx, &recordedHeight); err != nil {
+		return err
+	}
+
+	return nil
+}
+
 // CA -> x contract(EOA -> middle contracts -> x contract, internal transaction)
 func TracingContractEventCronInInternalTXN(ctx context.Context, contractAddress, eventHash, eventName string, callEventContentFn GetEventContentCallback) error {
 	var (
@@ -119,88 +201,6 @@ func TracingContractEventCronInInternalTXN(ctx context.Context, contractAddress,
 
 			// update or insert event_height_checkoutpoint
 			recordedHeight.MaxRecordedHeight = uint64(receiptTmp.Height)
-			if err := UpdateEventHeightCheckoutpoint(ctx, &recordedHeight); err != nil {
-				return err
-			}
-		}
-	}
-
-	recordedHeight.MaxRecordedHeight = uint64(maxHeightEvmReceipt.Height)
-	if err := UpdateEventHeightCheckoutpoint(ctx, &recordedHeight); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// EOA -> x contract(transaction)
-func TracingContractEventTXNCron(ctx context.Context, contractAddress, eventHash, eventName string, callEventContentFn GetEventContentCallback) error {
-	var (
-		maxHeightEvmReceipt fevm.EVMReceipt
-		recordedHeight      fevm.EventHeightCheckpoint
-	)
-	evmReceipts := make([]*fevm.EVMReceipt, 0)
-
-	recordedHeight.EventHash = eventHash
-	recordedHeight.EventName = eventName
-	recordedHeight.TxnType = TxnType
-
-	// select * from event_height_checkpoint where event_hash = %eventHash%;
-	// select max(height) from evm_receipt;
-	// select * from evm_receipt where height > leftHeight and height < rightHeight and logs like '%eventHash%' order by height desc;
-	if _, err := utils.X.Where("event_hash = ? and txn_type = ?", eventHash, TxnType).Get(&recordedHeight); err != nil {
-		log.Errorf("execute sql error: %v", err)
-		return err
-	}
-
-	if _, err := utils.X.Select("max(height) as height").Get(&maxHeightEvmReceipt); err != nil {
-		log.Errorf("execute sql error: %v", err)
-		return err
-	}
-
-	if maxHeightEvmReceipt.Height >= Finality {
-		maxHeightEvmReceipt.Height -= Finality
-	}
-
-	if err := utils.X.Where("height between ? and ? and \"to\" = ? and logs like ?", recordedHeight.MaxRecordedHeight+1, maxHeightEvmReceipt.Height, contractAddress, "%"+eventHash+"%").Asc("height").Find(&evmReceipts); err != nil {
-		log.Errorf("execute sql error: %v", err)
-		return err
-	}
-
-	for _, receipt := range evmReceipts {
-		logs := make([]ethtypes.EthLog, 0)
-		if err := json.Unmarshal([]byte(receipt.Logs), &logs); err != nil {
-			log.Warnf("Unmarshal receipt[height: %v] log err: %v", receipt.Height, err)
-			continue
-		}
-
-		for _, ethLog := range logs {
-			if ethLog.Topics[0].String() != eventHash {
-				continue
-			}
-
-			fevmEvent := fevm.FevmEvent{
-				ContractAddress: contractAddress,
-				Height:          uint64(receipt.Height),
-				TransactionHash: receipt.TransactionHash,
-				From:            receipt.From,
-				To:              receipt.To,
-				Status:          receipt.Status,
-				LogsBloom:       receipt.LogsBloom,
-				Logs:            receipt.Logs,
-				EventHash:       ethLog.Topics[0].String(),
-				EventName:       eventName,
-			}
-
-			fevmEvent.Note = callEventContentFn(eventName, &ethLog)
-
-			if _, err := utils.X.Insert(&fevmEvent); err != nil {
-				log.Errorf("execute sql error: %v", err)
-				return err
-			}
-
-			// update or insert event_height_checkoutpoint
-			recordedHeight.MaxRecordedHeight = uint64(receipt.Height)
 			if err := UpdateEventHeightCheckoutpoint(ctx, &recordedHeight); err != nil {
 				return err
 			}
